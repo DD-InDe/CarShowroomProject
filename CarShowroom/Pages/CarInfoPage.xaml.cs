5af679248fae324ef2fcd94c9e820e5fb18c569c
deer <bogdanovich_7712@mail.ru>
2024-12-20 21:28:48 +0300

    Finished ViewCarInfo

---
+++ CarShowroom/Pages/CarInfoPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CarShowroom.Database;
+using CarShowroom.Pages.EmployeePages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarShowroom.Pages;
+
+public partial class CarInfoPage : Page
+{
+    private Car _car;
+
+    public CarInfoPage(Car car)
+    {
+        _car = car;
+        InitializeComponent();
+
+        if (App.AuthorizedUser.RoleId is 3)
+            EditButton.Visibility = Visibility.Collapsed;
+        else
+            RequestButton.Visibility = Visibility.Collapsed;
+    }
+
+    private void CarInfoPage_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            _car = Db.Context.Cars
+                .Include(c => c.Model)
+                .Include(c => c.Model.Brand)
+                .Include(c => c.Model.EngineType)
+                .Include(c => c.Model.BodyType)
+                .Include(c => c.Model.Class)
+                .Include(c => c.Model.Generation)
+                .First(c => c.CarVin == _car.CarVin);
+            DataContext = _car;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            MessageBox.Show($"Произошла ошибка: {exception.Message}");
+        }
+    }
+
+    private void RequestButton_OnClick(object sender, RoutedEventArgs e) => throw new NotImplementedException();
+
+    private void EditButton_OnClick(object sender, RoutedEventArgs e)=>
+        NavigationService.Navigate(new EditCarPage(_car));
+}
--- CarShowroom/Pages/ViewCarPage.xaml.cs
+++ CarShowroom/Pages/ViewCarPage.xaml.cs
@@ -13,15 +13,9 @@ public partial class ViewCarPage : Page
         InitializeComponent();
     }
 
-    private void SearchTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
-    {
-        //todo сделать поиск
-    }
+    private void SearchTextBox_OnTextChanged(object sender, TextChangedEventArgs e) => LoadData();
 
-    private void BrandComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        //todo сделать фильтрацию
-    }
+    private void BrandComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) => LoadData();
 
     private void ViewCarPage_OnLoaded(object sender, RoutedEventArgs e)
     {
@@ -29,6 +23,10 @@ public partial class ViewCarPage : Page
         {
             CarPresenter.ItemsSource = Db.Context.Cars.Include(c => c.Model).Include(c => c.Model.Brand)
                 .Include(c => c.Status).ToList();
+
+            List<Brand> brands = new() { new() { Name = "Все" } };
+            brands.AddRange(Db.Context.Brands.ToList());
+            BrandComboBox.ItemsSource = brands;
         }
         catch (Exception exception)
         {
@@ -43,15 +41,16 @@ public partial class ViewCarPage : Page
         {
             List<Car> cars = Db.Context.Cars.Include(c => c.Model).Include(c => c.Model.Brand)
                 .Include(c => c.Status).ToList();
-            if (BrandComboBox.SelectedIndex != 0)
+            if (BrandComboBox.SelectedIndex != 0 && BrandComboBox.SelectedItem != null)
                 cars = cars.Where(c => c.Model.BrandId == ((Brand)BrandComboBox.SelectedItem).BrandId).ToList();
             cars = cars.Where(c =>
-                    c.Model.Name.ToLower().Contains(SearchTextBox.Text) ||
-                    c.Model.Brand.Name.ToLower().Contains(SearchTextBox.Text) ||
-                    c.YearOfManufacture.ToString().Contains(SearchTextBox.Text) ||
-                    c.Price.ToString().Contains(SearchTextBox.Text))
+                    c.Model.Name.ToLower().Contains(SearchTextBox.Text.ToLower()) ||
+                    c.Model.Brand.Name.ToLower().Contains(SearchTextBox.Text.ToLower()) ||
+                    c.YearOfManufacture.ToString().Contains(SearchTextBox.Text.ToLower()) ||
+                    c.Price.ToString().Contains(SearchTextBox.Text.ToLower()))
                 .ToList();
 
+            CarPresenter.ItemsSource = null;
             CarPresenter.ItemsSource = cars;
         }
         catch (Exception e)
@@ -63,6 +62,6 @@ public partial class ViewCarPage : Page
 
     private void AddButton_OnClick(object sender, RoutedEventArgs e) => NavigationService.Navigate(new EditCarPage());
 
-    private void EditButton_OnClick(object sender, RoutedEventArgs e) => NavigationService.Navigate(new EditCarPage(
+    private void InfoButton_OnClick(object sender, RoutedEventArgs e) => NavigationService.Navigate(new CarInfoPage(
         ((Button)sender).DataContext as Car));
 }
